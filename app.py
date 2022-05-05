3d0f28ea19a799417127d5e02fff606d7cc66217
daeunchung <jul12230103@gmail.com>
2022-05-06 00:23:08 +0900

    add index_mystar.html add api for mystar page (list, like, delete) add button for redirection

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ db = client.dbsparta
 
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def home_review():
+    return render_template('index_review.html')
 
-# API 역할을 하는 부분
+@app.route('/api')
+def home_mystar():
+    return render_template('index_mystar.html')
 
-
+# 리뷰 API 역할을 하는 부분
 @app.route('/review', methods=['POST'])
 def write_review():
     # 1) 클라이언트와 서버 확인
@@ -52,6 +54,38 @@ def read_reviews():
     reviews = list(db.bookreview.find({}, {'_id': False }))
     # 2. 성공 여부 & 리뷰 목록 반환
     return jsonify({'all_reviews': reviews})
+
+
+# mystar API 역할을 하는 부분
+# 뼈대 세우기
+
+@app.route('/api/list', methods=['GET'])
+def show_stars():
+    movie_star = list(db.mystar.find({}, {'_id': False}).sort('like',-1))
+    return jsonify({'movie_stars': movie_star})
+
+
+@app.route('/api/like', methods=['POST'])
+def like_star():
+    name_receive = request.form['name_give']
+
+    target_star = db.mystar.find_one({'name': name_receive})
+
+    current_like = target_star['like']
+    new_like = current_like + 1
+
+    db.mystar.update_one({'name': name_receive}, {'$set': {'like': new_like}})
+
+    return jsonify({'msg': '좋아요 완료!'})
+
+
+@app.route('/api/delete', methods=['POST'])
+def delete_star():
+    name_receive = request.form['name_give']
+    db.mystar.delete_one({'name': name_receive})
+
+    return jsonify({'msg': '삭제 완료!'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
